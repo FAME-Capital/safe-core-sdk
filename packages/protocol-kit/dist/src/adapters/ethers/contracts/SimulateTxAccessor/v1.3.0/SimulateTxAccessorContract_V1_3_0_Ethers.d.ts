33862c0c9c41814c53eea5be13f3add9b670900b
Nurfirliana Muzanella <muzanella11@gmail.com>
2023-12-01 15:32:47 +0700

    release build v0.0.1

--- packages/protocol-kit/dist/src/adapters/ethers/contracts/SimulateTxAccessor/v1.3.0/SimulateTxAccessorContract_V1_3_0_Ethers.d.ts
+++
@@ -1,7 +0,0 @@
-import { Simulate_tx_accessor as SimulateTxAccessor } from '@safe-global/protocol-kit/typechain/src/ethers-v5/v1.3.0/Simulate_tx_accessor';
-import SimulateTxAccessorEthersContract from '../SimulateTxAccessorEthersContract';
-declare class SimulateTxAccessorContract_V1_3_0_Ethers extends SimulateTxAccessorEthersContract {
-    contract: SimulateTxAccessor;
-    constructor(contract: SimulateTxAccessor);
-}
-export default SimulateTxAccessorContract_V1_3_0_Ethers;
